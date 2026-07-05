7210a1613e1852eb95c0ee8d7e29b02885ec2ef7
regiaodaareia <regiaodaareia@gmail.com>
2026-07-05 16:39:22 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// Tem Hoje na Região - Configuração do Firebase
+// Troque os dados abaixo pelos dados do seu projeto Firebase.
+// Firebase Console > Configurações do projeto > Seus apps > Web app.
+
+export const firebaseConfig = {
+  apiKey: "COLE_AQUI_API_KEY",
+  authDomain: "COLE_AQUI_AUTH_DOMAIN",
+  projectId: "COLE_AQUI_PROJECT_ID",
+  storageBucket: "COLE_AQUI_STORAGE_BUCKET",
+  messagingSenderId: "COLE_AQUI_MESSAGING_SENDER_ID",
+  appId: "COLE_AQUI_APP_ID"
+};
+
+// Coloque aqui o e-mail Google que poderá acessar o admin.
+// Exemplo: ["seuemail@gmail.com"]
+export const ADMIN_EMAILS = [
+  "COLE_SEU_EMAIL_ADMIN_AQUI"
+];
+
+// WhatsApp que recebe aviso de novo cadastro para aprovação.
+export const WHATSAPP_ADMIN = "5584998496406";
